64920d08c70b77937c01296676a2ef382b7089f6
logan4dog <logan4dog@gmail.com>
2022-11-05 03:18:51 -0400

    form code added

--- App.tsx
+++ App.tsx
@@ -18,6 +18,38 @@ function Child(){
   return <div>Child</div>
 }
 
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+
 export default function App() {
 
   console.log("Rendering App...");
@@ -34,6 +66,7 @@ export default function App() {
     <div className="App">
       {console.log("Before child.")}
       <Child></Child>
+      <NameForm />
       {console.log("After child.")}
     </div>
   );
